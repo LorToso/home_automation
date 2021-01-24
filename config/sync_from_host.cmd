f3f96e5dedb0da72efd9e840cae0f67421eea378
Lorenzo Toso <git@lorenzotoso.de>
2021-01-24 21:09:32 +0100

    Fixed sync script

--- config/sync_from_host.cmd
+++ config/sync_from_host.cmd
@@ -1,3 +1,3 @@
-scp -r root@192.168.2.139:/root/config/appdaemon/apps ../appdaemon/apps
+scp -r root@192.168.2.139:/root/config/appdaemon/apps ../appdaemon
 scp -r root@192.168.2.139:/root/config/appdaemon/appdaemon.yaml ../appdaemon/appdaemon.yaml
 scp -r root@192.168.2.139:/root/config/secrets.yaml ../secrets.yaml
