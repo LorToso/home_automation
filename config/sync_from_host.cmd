b10f12b0d6e3f2f78f07c0e022a24bc15ff38e04
Lorenzo Toso <git@lorenzotoso.de>
2021-01-24 20:51:23 +0100

    Added scripts to sync to and from PI

---
+++ config/sync_from_host.cmd
@@ -0,0 +1,2 @@
+scp -r root@192.168.2.139:/root/config/appdaemon/apps ../appdaemon/apps
+scp -r root@192.168.2.139:/root/config/appdaemon/appdaemon.yaml ../appdaemon/appdaemon.yaml
---
+++ config/sync_to_host.cmd
@@ -0,0 +1,2 @@
+scp -r ../appdaemon/apps root@192.168.2.139:/root/config/appdaemon/apps
+scp ../appdaemon/appdaemon.yaml root@192.168.2.139:/root/config/appdaemon/appdaemon.yaml
