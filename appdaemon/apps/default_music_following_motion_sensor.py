b5b2959c0eadf7b48c87f206b9809a7efd4d339a
Lorenzo Toso <git@lorenzotoso.de>
2021-02-13 01:24:48 +0100

    Follow music motion sensor now works :+1:

--- appdaemon/apps/default_music_following_motion_sensor.py
+++ appdaemon/apps/default_music_following_motion_sensor.py
@@ -11,13 +11,19 @@ class MusicFollowingMotionSensor(LightControllingMotionSensor):
     turn_off_after_seconds: int = 0
     activation_boolean: Optional[InputBoolean] = None
     motion_added_speaker_in_group: Optional[bool] = None
+    music_follow_boolean: Optional[InputBoolean] = None
 
     def initialize(self) -> None:
         self.speaker = self.get_app(self.args["speaker_app_name"])
+        self.music_follow_boolean = self.get_app(self.args["music_following_input_boolean_app_name"])
         super().initialize()
 
     def on_immediate_on(self) -> None:
         super().on_immediate_on()
+
+        if self.music_follow_boolean.is_off():
+            self.log(f"music_follow_boolean is off. Doing nothing.")
+            return
 
         if self.speaker.is_playing():
             self.log("Already playing. Doing nothing.")
@@ -32,6 +38,10 @@ class MusicFollowingMotionSensor(LightControllingMotionSensor):
 
     def on_delayed_off(self) -> None:
         super().on_delayed_off()
+
+        if self.music_follow_boolean.is_off():
+            self.log(f"music_follow_boolean is off. Doing nothing.")
+            return
 
         if self.motion_added_speaker_in_group:
             self.log("Delayed off. Speaker unjoining group.")
