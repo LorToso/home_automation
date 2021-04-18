f74be41ad614b5beaa227384e2a86b0d4ea326aa
Lorenzo Toso <git@lorenzotoso.de>
2021-04-19 00:23:11 +0200

    Added a boolean that makes sure the first time of the day the night lamp turns on bright instead of red

--- appdaemon/apps/input_boolean/boolean_set.py
+++ appdaemon/apps/input_boolean/boolean_set.py
@@ -2,7 +2,6 @@ from typing import Union, List, Tuple
 
 import appdaemon.plugins.hass.hassapi as hass
 
-from helpers import safe_get_app
 from input_boolean import InputBoolean
 
 
@@ -17,18 +16,21 @@ class BooleanSet:
         self.negatives = negatives
 
     def is_active(self):
-        return all([b.is_on() for b in self.positives]) and not any([b.is_on() for b in self.negatives])
+        return all([self.is_boolean_on(b) for b in self.positives]) and not any([self.is_boolean_on(b) for b in self.negatives])
 
     def log_states(self):
         positives_str = ', '.join(
-            [f'{boolean.entity_id}: {boolean.state}' for boolean in self.positives]
+            [f'{boolean}: {self.controller.get_state(boolean)}' for boolean in self.positives]
         )
         negatives_str = ', '.join(
-            [f'{boolean.entity_id}: {boolean.state}' for boolean in self.negatives]
+            [f'{boolean}: {self.controller.get_state(boolean)}' for boolean in self.negatives]
         )
 
         full_str = f"Boolean states: Positives: [{positives_str}], Negatives: [{negatives_str}]"
         self.controller.log(full_str)
+
+    def is_boolean_on(self, entity_id: str):
+        return self.controller.get_state(entity_id) == 'on'
 
     @staticmethod
     def parse_activation_boolean(
@@ -47,15 +49,9 @@ class BooleanSet:
             b for b in activation_boolean if not b.startswith(BooleanSet.NEGATION_PREFIX)
         ]
         negatives = [
-            b for b in activation_boolean if b not in positives
+            b[len(BooleanSet.NEGATION_PREFIX):] for b in activation_boolean if b not in positives
         ]
         controller.log(f"Positives: {positives}")
         controller.log(f"Negatives: {negatives}")
 
-        positives = [
-            safe_get_app(controller, b) for b in positives
-        ]
-        negatives = [
-            safe_get_app(controller, b[len(BooleanSet.NEGATION_PREFIX):]) for b in negatives
-        ]
         return positives, negatives
