5ab2cdd45b53cca76da7075ffabd0e6b9294613e
Lorenzo Toso <git@lorenzotoso.de>
2021-10-12 00:06:55 +0200

    Split automations into multiple files. Updated dashboard

--- config/Xcopy_automations_to_host.cmd
+++ config/Xcopy_automations_to_host.cmd
@@ -6,3 +6,4 @@ scp ../custom_lights.yaml root@192.168.4.139:/root/config/custom_lights.yaml
 scp ../configuration.yaml root@192.168.4.139:/root/config/configuration.yaml
 scp ../secrets.yaml root@192.168.4.139:/root/config/secrets.yaml
 scp ../groups.yaml root@192.168.4.139:/root/config/groups.yaml
+scp -r ../automations/* root@192.168.4.139:/root/config/automations/
