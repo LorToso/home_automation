cb04ddb65932ae9e991fca08829db0bb42ffa30d
Lorenzo Toso <git@lorenzotoso.de>
2021-11-02 22:07:10 +0100

    Added all important input booleans as config files Adjsuted all presence automations Renamed Dashboard file such that is correctly picked up by HA

--- config/Xcopy_automations_to_host.cmd
+++ config/Xcopy_automations_to_host.cmd
@@ -7,3 +7,4 @@ scp ../configuration.yaml root@192.168.4.139:/root/config/configuration.yaml
 scp ../secrets.yaml root@192.168.4.139:/root/config/secrets.yaml
 scp ../groups.yaml root@192.168.4.139:/root/config/groups.yaml
 scp -r ../automations/* root@192.168.4.139:/root/config/automations/
+scp -r ../input_booleans/* root@192.168.4.139:/root/config/input_booleans/
