4063641c3c4b0980726aaf324b344562730f1c87
Lorenzo Toso <git@lorenzotoso.de>
2021-11-02 23:17:15 +0100

    Split Scenes into different files

--- config/Xcopy_automations_to_host.cmd
+++ config/Xcopy_automations_to_host.cmd
@@ -1,4 +1,3 @@
-scp ../scenes.yaml root@192.168.4.139:/root/config/scenes.yaml
 scp ../scripts.yaml root@192.168.4.139:/root/config/scripts.yaml
 scp ../custom_sensors.yaml root@192.168.4.139:/root/config/custom_sensors.yaml
 scp ../custom_lights.yaml root@192.168.4.139:/root/config/custom_lights.yaml
@@ -9,3 +8,4 @@ scp ../ui-lovelace.yaml root@192.168.4.139:/root/config/ui-lovelace.yaml
 scp -r ../automations/* root@192.168.4.139:/root/config/automations/
 scp -r ../input_booleans/* root@192.168.4.139:/root/config/input_booleans/
 scp -r ../dashboards/* root@192.168.4.139:/root/config/dashboards/
+scp -r ../scenes/* root@192.168.4.139:/root/config/scenes/
