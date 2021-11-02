af621909b0d8f7c34775a4aa2f5728fe0fd33872
Lorenzo Toso <git@lorenzotoso.de>
2021-11-02 23:05:01 +0100

    Split up dashboard file into single cards and made it work this time

--- config/Xcopy_automations_to_host.cmd
+++ config/Xcopy_automations_to_host.cmd
@@ -1,4 +1,3 @@
-scp ../automations.yaml root@192.168.4.139:/root/config/automations.yaml
 scp ../scenes.yaml root@192.168.4.139:/root/config/scenes.yaml
 scp ../scripts.yaml root@192.168.4.139:/root/config/scripts.yaml
 scp ../custom_sensors.yaml root@192.168.4.139:/root/config/custom_sensors.yaml
@@ -6,5 +5,7 @@ scp ../custom_lights.yaml root@192.168.4.139:/root/config/custom_lights.yaml
 scp ../configuration.yaml root@192.168.4.139:/root/config/configuration.yaml
 scp ../secrets.yaml root@192.168.4.139:/root/config/secrets.yaml
 scp ../groups.yaml root@192.168.4.139:/root/config/groups.yaml
+scp ../ui-lovelace.yaml root@192.168.4.139:/root/config/ui-lovelace.yaml
 scp -r ../automations/* root@192.168.4.139:/root/config/automations/
 scp -r ../input_booleans/* root@192.168.4.139:/root/config/input_booleans/
+scp -r ../dashboards/* root@192.168.4.139:/root/config/dashboards/
