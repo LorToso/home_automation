684fa027cacf199e2856fc2fc84debe10b001c2d
Lorenzo Toso <LorToso@gmail.com>
2023-05-14 23:58:33 +0200

    Added timers to better control how things work with multiple people in the house

--- config/sync/copy_helpers.cmd
+++ config/sync/copy_helpers.cmd
@@ -3,3 +3,4 @@ scp -pr ../../input_texts/* root@192.168.4.139:/root/config/input_texts/
 scp ../../custom_binary_sensors.yaml root@192.168.4.139:/root/config/custom_binary_sensors.yaml
 scp ../../custom_sensors.yaml root@192.168.4.139:/root/config/custom_sensors.yaml
 scp ../../custom_lights.yaml root@192.168.4.139:/root/config/custom_lights.yaml
+scp ../../custom_timers.yaml root@192.168.4.139:/root/config/custom_timers.yaml
