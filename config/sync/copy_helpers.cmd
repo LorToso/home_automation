30ce77b5ed5b47cf688d3b23223d0cd3f56cfb35
Lorenzo Toso <LorToso@gmail.com>
2023-11-16 22:11:36 +0100

    Split custom sensors into separate files

--- config/sync/copy_helpers.cmd
+++ config/sync/copy_helpers.cmd
@@ -1,5 +1,6 @@
 scp -pr ../../input_booleans/* root@192.168.4.139:/root/config/input_booleans/
 scp -pr ../../input_texts/* root@192.168.4.139:/root/config/input_texts/
+scp -pr ../../sensors/* root@192.168.4.139:/root/config/sensors/
 scp ../../custom_binary_sensors.yaml root@192.168.4.139:/root/config/custom_binary_sensors.yaml
 scp ../../custom_sensors.yaml root@192.168.4.139:/root/config/custom_sensors.yaml
 scp ../../custom_lights.yaml root@192.168.4.139:/root/config/custom_lights.yaml
