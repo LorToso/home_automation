518bd1588f5f9f1364d1f426d749b146e295c8a3
Lorenzo Toso <LorToso@gmail.com>
2024-02-13 23:56:52 +0100

    Deleted appdaemon directory (was only bothering the IDE and it is still in git)

--- appdaemon/apps/config/strings.py
+++
@@ -1,6 +0,0 @@
-from typing import Tuple
-
-motion_sensor_deactivated_message: Tuple[str, str] = ('Motion sensor deactivated', 'en')
-motion_sensor_activated_message: Tuple[str, str] = ('Motion sensor activated', 'en')
-
-guest_mode_activation_notification_message = "Do you want to activate guest mode?"
