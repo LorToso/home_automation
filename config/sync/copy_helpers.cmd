13bbae55801b115703aec68475fa3d38fa36a9c8
Lorenzo Toso <LorToso@gmail.com>
2024-02-18 18:19:47 +0100

    Added test universal media player

--- config/sync/copy_helpers.cmd
+++ config/sync/copy_helpers.cmd
@@ -5,6 +5,7 @@ scp -o MACs=hmac-sha2-512-etm@openssh.com -pr ../../binary_sensors/* root@192.16
 scp -o MACs=hmac-sha2-512-etm@openssh.com -pr ../../counters/* root@192.168.178.26:/root/config/counters/
 scp -o MACs=hmac-sha2-512-etm@openssh.com -pr ../../input_numbers/* root@192.168.178.26:/root/config/input_numbers/
 scp -o MACs=hmac-sha2-512-etm@openssh.com -pr ../../timers/* root@192.168.178.26:/root/config/timers/
+scp -o MACs=hmac-sha2-512-etm@openssh.com ../../custom_media_players.yaml root@192.168.178.26:/root/config/custom_media_players.yaml
 scp -o MACs=hmac-sha2-512-etm@openssh.com ../../custom_binary_sensors.yaml root@192.168.178.26:/root/config/custom_binary_sensors.yaml
 scp -o MACs=hmac-sha2-512-etm@openssh.com ../../custom_sensors.yaml root@192.168.178.26:/root/config/custom_sensors.yaml
 scp -o MACs=hmac-sha2-512-etm@openssh.com ../../custom_lights.yaml root@192.168.178.26:/root/config/custom_lights.yaml
